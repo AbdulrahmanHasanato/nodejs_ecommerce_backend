33284a4a7d9e09576815b9e0a8e623ef8ac3e1d5
Abdulrahman <byjyta@gmail.com>
2024-01-17 12:28:50 +0300

    new commit

--- services/orderService.js
+++ services/orderService.js
@@ -13,28 +13,32 @@ const User = require("../models/userModel");
 // @route   POST /api/v1/orders/cartId
 // @access  Protected/User
 exports.createCashOrder = asyncHandler(async (req, res, next) => {
-    //App settings
+    // app settings
     const taxPrice = 0;
     const shippingPrice = 0;
 
-    //1- Get cart depending on cartId
+    // 1) Get cart depend on cartId
     const cart = await Cart.findById(req.params.cartId);
-    if (!cart) return next(new ApiError(`There's no cart with ID: ${req.params.cartId}`, 404));
+    if (!cart) {
+        return next(new ApiError(`There is no such cart with id ${req.params.cartId}`, 404));
+    }
 
-    //2- Get order price depending on cart price "check if coupon is applied"
+    // 2) Get order price depend on cart price "Check if coupon apply"
     const cartPrice = cart.totalPriceAfterDiscount
         ? cart.totalPriceAfterDiscount
         : cart.totalCartPrice;
+
     const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
 
-    //3- Create order with default payment method (cash)
+    // 3) Create order with default paymentMethodType cash
     const order = await Order.create({
         user: req.user._id,
         cartItems: cart.cartItems,
         shippingAddress: req.body.shippingAddress,
         totalOrderPrice,
     });
-    //4- After creating order, decrease product quantity and increase product sold quantity
+
+    // 4) After creating order, decrement product quantity, increment product sold
     if (order) {
         const bulkOption = cart.cartItems.map((item) => ({
             updateOne: {
@@ -44,14 +48,11 @@ exports.createCashOrder = asyncHandler(async (req, res, next) => {
         }));
         await Product.bulkWrite(bulkOption, {});
 
-        //5- Clear cart depending on cartId
+        // 5) Clear cart depend on cartId
         await Cart.findByIdAndDelete(req.params.cartId);
     }
 
-    res.status(201).json({
-        success: "success",
-        data: order,
-    });
+    res.status(201).json({ status: "success", data: order });
 });
 
 exports.filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
@@ -113,65 +114,65 @@ exports.updateOrderToDelivered = asyncHandler(async (req, res, next) => {
 // @route   GET /api/v1/orders/checkout-session/cartId
 // @access  Protected/User
 exports.checkoutSession = asyncHandler(async (req, res, next) => {
-    //App settings
+    // app settings
     const taxPrice = 0;
     const shippingPrice = 0;
 
-    //1- Get cart depending on cartId
+    // 1) Get cart depend on cartId
     const cart = await Cart.findById(req.params.cartId);
-    if (!cart) return next(new ApiError(`There's no cart with ID: ${req.params.cartId}`, 404));
+    if (!cart) {
+        return next(new ApiError(`There is no such cart with id ${req.params.cartId}`, 404));
+    }
 
-    //2- Get order price depending on cart price "check if coupon is applied"
+    // 2) Get order price depend on cart price "Check if coupon apply"
     const cartPrice = cart.totalPriceAfterDiscount
         ? cart.totalPriceAfterDiscount
         : cart.totalCartPrice;
-    const totalOrderPrice = cartPrice;
 
-    //3- Create stripe checout session
+    const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+
+    // 3) Create stripe checkout session
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
-                price_data: {
-                    currency: "try",
-                    unit_amount: totalOrderPrice * 100,
-                    product_data: {
-                        name: req.user.name,
-                    },
-                },
+                name: req.user.name,
+                amount: totalOrderPrice * 100,
+                currency: "try",
                 quantity: 1,
             },
         ],
         mode: "payment",
-        success_url: `${req.protocol}://${req.get("host")}/api/v1/orders`,
-        cancel_url: `${req.protocol}://${req.get("host")}/api/v1/cart`,
+        success_url: `${req.protocol}://${req.get("host")}/orders`,
+        cancel_url: `${req.protocol}://${req.get("host")}/cart`,
         customer_email: req.user.email,
         client_reference_id: req.params.cartId,
         metadata: req.body.shippingAddress,
     });
-    //4- Send session to response
+
+    // 4) send session to response
     res.status(200).json({ status: "success", session });
 });
 
 const createCardOrder = async (session) => {
     const cartId = session.client_reference_id;
     const shippingAddress = session.metadata;
-    const orderPrice = session.display_items[0].amount / 100;
+    const oderPrice = session.amount_total / 100;
 
     const cart = await Cart.findById(cartId);
-    const user = User.findOne({ email: session.customer_email });
+    const user = await User.findOne({ email: session.customer_email });
 
-    //3- Create order with default payment method (card)
+    // 3) Create order with default paymentMethodType card
     const order = await Order.create({
         user: user._id,
         cartItems: cart.cartItems,
         shippingAddress,
-        totalOrderPrice: orderPrice,
+        totalOrderPrice: oderPrice,
         isPaid: true,
         paidAt: Date.now(),
         paymentMethodType: "card",
     });
 
-    //4- After creating order, decrease product quantity and increase product sold quantity
+    // 4) After creating order, decrement product quantity, increment product sold
     if (order) {
         const bulkOption = cart.cartItems.map((item) => ({
             updateOne: {
@@ -181,7 +182,7 @@ const createCardOrder = async (session) => {
         }));
         await Product.bulkWrite(bulkOption, {});
 
-        //5- Clear cart depending on cartId
+        // 5) Clear cart depend on cartId
         await Cart.findByIdAndDelete(cartId);
     }
 };
@@ -200,7 +201,7 @@ exports.webhookCheckout = asyncHandler(async (req, res, next) => {
         return res.status(400).send(`Webhook Error: ${err.message}`);
     }
     if (event.type === "checkout.session.completed") {
-        //Create order
+        // Create order
         createCardOrder(event.data.object);
     }
 
